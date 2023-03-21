c8f03333b5daf35168fa5911421e9ba57a17fb17
Jonas324 <127858261+Mind-Bender@users.noreply.github.com>
2023-03-21 21:01:11 +0100

    made some functions

---
+++ datatype.js
@@ -0,0 +1,26 @@
+const greet = "Hello Everybody";
+console.log(greet);
+
+const project = {
+    option1: 'E-store,',
+    option2: 'Bank',
+    start: function() {
+        console.log(this)
+        console.log(`Started ${this.option1} ${this.option2}`)},
+    stop: function() {
+        console.log(this)
+        console.log(`Stopped ${this.option1} ${this.option2}`)}
+}
+project.start()
+project.stop()
+
+addItem = () => {
+        let item1 = 'shoes';
+        let item2 = 'pants';
+        let item3 ='shirt';
+        console.log(item1);
+        console.log(item2);
+        console.log(item3);
+}
+
+addItem();
